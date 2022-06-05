e139e9eaff0a9723720c46783d5ac16ab975384a
asami0119 <assa.0119.0425.0905@gmail.com>
2022-06-05 08:40:23 -0400

    Showing Current Temperature/JS

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let descriptionElement = document.querySelector("#description");
 
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
+
+let apiKey = "01e6c99e19f4b3595e992027a30a8417";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Mississauga&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+console.log(apiKey);
+axios.get(apiUrl).then(displayTemperature);
